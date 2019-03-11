8ac7f6be893560ff6d65f1d312a71919d3150215
Alex <alex.isayenko@gmail.com>
2019-03-11 19:46:10 +0200

    webservice hello world

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("Yo yo yo yo!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8011", nil)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+
+	fmt.Println(text)
+}
